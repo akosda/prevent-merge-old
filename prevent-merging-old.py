7da22871be86a8661c503264a0e82f471c5384f6
Akos Fabian <akos.fabian@digitalasset.com>
2017-03-29 10:21:56 +0200

    Refactor and ascii encode text

--- prevent-merging-old.py
+++ prevent-merging-old.py
@@ -5,7 +5,7 @@ import json
 def auth_headers(username, password):
     return 'Basic ' + base64.encodestring('%s:%s' % (username, password))[:-1]
 
-def jenkins_json(path):
+def jenkins_json_response(path):
     auth = auth_headers('akoslocal', '623b34224508efb9bad027a7d29e1b3e')
     top_level_url = "http://localhost:8080"
     a_url = "%s/%s" % (top_level_url, path)
@@ -13,7 +13,7 @@ def jenkins_json(path):
     req.add_header('Authorization', auth)
     return json.load(urllib2.urlopen(req))
 
-def blue(job):
+def is_blue_job(job):
     colorKey = u'color'
     if colorKey in job:
         if job[colorKey] == u'blue':
@@ -24,10 +24,10 @@ def blue(job):
         return False
 
 def main():
-    j = jenkins_json('/api/json?pretty=true')
+    j = jenkins_json_response('/api/json?pretty=true')
     jobs = j["jobs"]
-    blueJobs = [j for j in jobs if blue(j)]
-    blueUrls = [j[u'url'] for j in blueJobs]
+    blueJobs = [j for j in jobs if is_blue_job(j)]
+    blueUrls = [j[u'url'].encode('ascii') for j in blueJobs]
     print blueUrls
 
 main()
